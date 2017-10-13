8c5ad22eece4d8ffcfa7d88e9e71564a4bee035f
Carlos Costa <jr.carlosad@gmail.com>
2017-10-13 00:20:22 -0300

    refactor: make more stateless function

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book'
 
-class AllBooks extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        onUpdateBook: PropTypes.func.isRequired
-    }
+const AllBooks = (props) => {
+    const books = props.books;
 
-    state = {
-        value: ""
-    }
-
-    render() {
-        const books = this.props.books;
-        return (
-            <div className="bookshelf">
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books.map(book => (
-                            <Book key={book.id} book={book} onMoveBook={this.props.onUpdateBook} />
-                        ))}
-                    </ol>
-                </div>
+    return (
+        <div className="bookshelf">
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map(book => (
+                        <Book key={book.id} book={book} onMoveBook={props.onUpdateBook} />
+                    ))}
+                </ol>
             </div>
-        );
-    }
+        </div>
+    );
+}
+
+AllBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    onUpdateBook: PropTypes.func.isRequired
 }
 
 export default AllBooks;
