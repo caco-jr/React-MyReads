a51d5f795617e57f9db56d3b056df6ae52625161
Carlos Costa <jr.carlosad@gmail.com>
2017-10-29 16:08:19 -0200

    feat: Making anchor links

--- src/components/NavLinks/index.js
+++ src/components/NavLinks/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Scrollchor from 'react-scrollchor';
-import { SHELVES } from '../utils/shelves'
+import { SHELVES } from '../../utils/shelves'
+import './navLinks.css'
+import Sticky from '../Sticky'
 
 const NavLinks = (props) => {
     return (
-        <nav className="section__links">
-            <ul>
+        <Sticky className="sticky__nav section__container">
+            <ul className="section__list">
+                <li className="section__list--links">
+                    <span> Go to section: </span>
+                </li>
+
                 {
                     SHELVES.map(section => (
-                        <li key={section.flag}>
+                        <li key={section.flag} className="section__list--links">
                             <Scrollchor to={section.flag}>
                                 {section.shelf}
                             </Scrollchor>
@@ -16,7 +22,7 @@ const NavLinks = (props) => {
                     ))
                 }
             </ul>
-        </nav>
+        </Sticky>
     )
 }
 
